0fd60a008ccb27a12bdcae3084ec4b2714414114
Pratik Mahesh Marudwar <pratik.marudwarmit@gmail.com>
2026-07-24 23:33:12 +0530

    .

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -107,6 +107,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
 
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
